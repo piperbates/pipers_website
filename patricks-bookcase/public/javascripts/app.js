8b07c199e3327b80cbf31fe4885f8c8aa0fa87be
Piper <bates.sarah42@gmail.com>
2020-11-19 22:03:57 +0000

    take 2

--- patricks-bookcase/public/javascripts/app.js
+++
@@ -1,8 +0,0 @@
-const table = document.querySelector("#db");
-const button = document.querySelector("#display")
-
-let mainTitle = "Library of Patrick"
-
-document.title = mainTitle + " : " + document.title;
-
-window.addEventListener("load", getTable)
--- patricks-bookcase/readme.txt
+++
@@ -1,5 +0,0 @@
-***To Do***
-
-Update so Patrick can update the db using the front end
-
-A HTML form that takes the data from the inputs and stores them in variables
