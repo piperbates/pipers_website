17ed36a3ff6d34ec538aca65a4f3f305e63bc7a1
Piper <bates.sarah42@gmail.com>
2020-12-12 01:24:22 +0000

    background scribbles and other personalisation touches

--- src/components/pages/About/index.js
+++ src/components/pages/About/index.js
@@ -1,16 +1,38 @@
 import React from "react";
 import "./style.css";
-import "../../../box-style.css"
+import "../../../box-style.css";
 
 export default function About() {
-  return (<div className="anchor-box">
-  <a name="about" href="/#">&nbsp;</a>
-     <div id="about-container" className="box">
-      <div id="about-box">
-    
-        <h1 id="about-h1">This is an about page</h1>
+  return (
+    <div className="anchor-box">
+      <a name="about" href="/#">
+        &nbsp;
+      </a>
+      <div id="about-container" className="box">
+        <div id="about-box">
+          <h1 id="about-h1">This is an about page</h1>
+
+          <p>
+            Hi, I'm Piper and I'm passionate about creativity. I'm currently on
+            the School of Code bootcamp, where I'm learning all kinds of
+            wonderful things to do with code and I'm loving every minute of it
+            so far!
+          </p>
+
+          <p>
+            Hi, I'm Piper and I'm passionate about creativity. I'm currently on
+            the School of Code bootcamp, where I'm learning all kinds of
+            wonderful things to do with code and I'm loving every minute of it
+            so far!
+          </p>
+          <p>
+            Hi, I'm Piper and I'm passionate about creativity. I'm currently on
+            the School of Code bootcamp, where I'm learning all kinds of
+            wonderful things to do with code and I'm loving every minute of it
+            so far!
+          </p>
+        </div>
       </div>
     </div>
-  </div>
   );
 }
