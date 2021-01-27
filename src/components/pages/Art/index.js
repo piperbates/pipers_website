f8a4e85685e8c8fcd4976e174bb7ee089a9a8e8a
Piper <bates.sarah42@gmail.com>
2021-01-27 11:30:54 +0000

    adding more art

--- src/components/pages/Art/index.js
+++ src/components/pages/Art/index.js
@@ -15,7 +15,7 @@ export default function Art() {
       <div id="art-container" className="box">
         <div id="art-box">
           <h2>Art and Illustration</h2>
-          <p>I've always loved to draw, and spent 5 years as a freelance illustrator creating children's books, branding illustration, portraits and more. Here's some of my favourite pieces I've worked on!</p>
+          <p>I've always loved to draw, and spent 5 years as a freelance illustrator creating children's books, branding illustration, portraits and more. Here's some of my favourite pieces I've worked on! If you want to see more of my illustration work, you can find my illustration portfolio here: <a href="http://www.piperstrange.com">PiperStrange.com</a></p>
           <div id="gallery" className="misc-gallery">
             {galleryArr.map((a) => {
               return <GalleryItem img={a} />;
