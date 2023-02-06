c0e21f98e29ca0af92be14abbe03ed6f94862108
work <piper@steel-buy.com>
2023-02-06 01:38:46 +0000

    typo

--- src/content/pageContent/LandingPageContent.tsx
+++ src/content/pageContent/LandingPageContent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const LandingPageContent = () => (<p>
-                An experienced software engineer an eye for UI/UX design. <br />
+                An experienced software engineer with an eye for UI/UX design. <br />
 
                 Currently working as a Frontend Engineer for SteelBuy.
             </p>)
