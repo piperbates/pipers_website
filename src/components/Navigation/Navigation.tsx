ae13d8519ab226c0bba7b2681c702cbadcbe9be2
work <piper@steel-buy.com>
2023-02-06 00:53:48 +0000

    modal close on click outside and responsiveness fix

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,32 +1,47 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Link, Route } from "wouter";
+import useOnClickOutside from '../../hooks/useOnClickOutside';
 
 import { PageTitles, PageValues } from '../../PageValues';
 import './Navigation.scss'
 
 export const Navigation = () => {
+    const [navOpen, setNavOpen] = useState(false);
+
+    const navRef = useRef<HTMLLIElement>(null);
+
+    // useOnClickOutside(navRef, ()=> {setNavOpen(false)})
+    
+
    return <nav>
+        
+        <button onClick={()=>{
+                setNavOpen(!navOpen);
+            }}>
+            <i className="fas fa-solid fa-bars"></i>
+        </button>
+        <div className={navOpen ? 'nav__menu-open' : 'nav__menu-closed'}>
         <ul>
             <Route path="/">
-                <li>
+                <li ref={navRef}>
                     <a href={`#${PageValues.TOP}`}>
                         <i className="fas fa-home icon"/>
                         Home
                     </a>
                 </li>
-                <li>
+                <li ref={navRef}>
                     <a href={`#${PageValues.PROJECTS}`}>
                         <i className="fas fa-code icon"/>
                         {PageTitles.PROJECTS}
                     </a>
                 </li>
-                <li>
+                <li ref={navRef}>
                     <a href={`#${PageValues.ABOUT}`}>
                         <i className="fas fa-grin-beam icon"/>
                         {PageTitles.ABOUT}
                     </a>
                 </li>
-                <li>
+                <li ref={navRef}>
                     <a href={`#${PageValues.CREATIVE}`}>
                         <i className="fas fa-paint-brush icon"/>
                         {PageTitles.CREATIVE}
@@ -49,5 +64,6 @@ export const Navigation = () => {
 
             </Route>
         </ul>
+        </div>
     </nav>
 }
